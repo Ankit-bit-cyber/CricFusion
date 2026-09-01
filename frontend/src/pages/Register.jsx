6a5dae6af53de5dd4ae417fbae4265d502c0edd7
Ankit-bit-cyber <208ankitkumarssm@gmail.com>
2026-09-01 13:33:38 +0530

    feat: refactor Profile component to Register with authentication layout

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,35 +1,10 @@
-import { useState } from 'react';
-import { useParams } from 'react-router-dom';
-import MainLayout from '../layouts/MainLayout';
-import ProfileHeader from '../components/profile/ProfileHeader';
-import UserPosts from '../components/profile/UserPosts';
-import EditProfile from '../components/profile/EditProfile';
-import Modal from '../components/common/Modal';
-import useFetch from '../hooks/useFetch';
-import { getUserApi } from '../services/userApi';
+import AuthLayout from '../layouts/AuthLayout';
+import RegisterForm from '../components/auth/RegisterForm';
 
-const Profile = () => {
-  const { id } = useParams();
-  const { data, loading, refetch } = useFetch(() => getUserApi(id), [id]);
-  const [editOpen, setEditOpen] = useState(false);
+const Register = () => (
+  <AuthLayout>
+    <RegisterForm />
+  </AuthLayout>
+);
 
-  return (
-    <MainLayout>
-      <div className="space-y-5">
-        <ProfileHeader
-          user={data?.user}
-          onEdit={() => setEditOpen(true)}
-          onFollowChange={refetch}
-        />
-        <h2 className="text-sm font-medium text-brand-600 px-1">Posts</h2>
-        <UserPosts posts={data?.posts} loading={loading} />
-      </div>
-
-      <Modal isOpen={editOpen} onClose={() => setEditOpen(false)} title="Edit Profile">
-        <EditProfile onClose={() => { setEditOpen(false); refetch(); }} />
-      </Modal>
-    </MainLayout>
-  );
-};
-
-export default Profile;
+export default Register;
